f778a464fd7ba4d9fc2e710c7d69997e5c71e627
Mr. Cain <58246556+DrDrewCain@users.noreply.github.com>
2023-05-12 14:05:16 -0400

    Add and comment out the test

--- src/GameFile/Test_cases/Test.ts
+++ src/GameFile/Test_cases/Test.ts
@@ -1,36 +1,37 @@
-import { EntityManager, createConnection } from 'typeorm';
-import { Player } from '../UserFiles/player.entity';
+// import { EntityManager } from 'typeorm';
+// import { Player } from '../UserFiles/player.entity';
+// import { appDataSource } from '../GameFile/serverStarter';
 
-describe('Player Entity', () => {
-  let connection: EntityManager;
+// describe('Player Entity', async () => {
+//     let connection = await EntityManager.getDataSource().initialize();
 
-  beforeAll(async () => {
-    connection = await createConnection({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'user',
-      password: 'password',
-      database: 'test',
-      entities: [Player],
-      synchronize: true,
-    });
-  });
+//   beforeAll(async () => {
+//     connection = await createConnection({
+//       type: 'postgres',
+//       host: 'localhost',
+//       port: 5432,
+//       username: 'user',
+//       password: 'password',
+//       database: 'test',
+//       entities: [Player],
+//       synchronize: true,
+//     });
+//   });
 
-  afterAll(async () => {
-    await connection.close();
-  });
+//   afterAll(async () => {
+//     await connection.close();
+//   });
 
-  it('should insert a new player', async () => {
-    const player = new Player();
-    player.name = 'John Doe';
-    player.score = 100;
+//   it('should insert a new player', async () => {
+//     const player = new Player();
+//     player.name = 'John Doe';
+//     player.score = 100;
 
-    await connection.save(player);
+//     await connection.save(player);
 
-    expect(player).toBeDefined();
-    expect(player.id).toBeDefined();
-    expect(player.name).toBe('John Doe');
-    expect(player.score).toBe(100);
-  });
-});
+//     expect(player).toBeDefined();
+//     expect(player.id).toBeDefined();
+//     expect(player.name).toBe('John Doe');
+//     expect(player.score).toBe(100);
+//   });
+// });
