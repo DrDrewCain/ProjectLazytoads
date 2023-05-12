223439626ee75af8e2aede26d17934e9a8f3dd86
Mr. Cain <58246556+DrDrewCain@users.noreply.github.com>
2023-05-12 19:21:04 -0400

    Update Styling

---
+++ WarriorsFront/src/app/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+  const currentYear = new Date().getFullYear()
+
+  return (
+    <footer className="flex items-center justify-center w-full h-16 bg-max-w-5xl">
+      <p className="text-max-w-5xl-600">
+        &copy; {currentYear} Your Website Name. All rights reserved.
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
--- WarriorsFront/src/app/page.tsx
+++ WarriorsFront/src/app/page.tsx
@@ -1,13 +1,29 @@
-import Image from 'next/image'
+import React from 'react';
+import Footer from './components/Footer';
 
-export default function Home() {
+const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:mx-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 lg:text-center">
-          DEVELOPMENT IN PROGRESS..... LAZYTOADS ARE WORKING ON IT
-        </p>
+    <div className="min-h-screen flex flex-col">
+      <main className="bg-max-w-5xl bg-opacity-50 min-h-screen flex-1 py-20">
+        <div className="max-w-5xl mx-auto flex flex-col items-center justify-center text-center">
+          <h1 className="text-max-w-5xl text-5xl font-bold tracking-wider mb-6 font-style: italic">
+            Lazytoads
+          </h1>
+          <p className="text-black text-lg mb-10">
+            Currently working hard to bring you the best website possible. Release: 2024
+          </p>
+          <p className="fixed left-0 top-0 w-full justify-center border-b border-max-w-5xl-300 bg-max-w-5xl bg-opacity-50 pb-6 pt-8 backdrop-blur-3xl text-black font-bold tracking-wider lg:static lg:mx-auto lg:rounded-xl lg:border lg:max-w-5xly-200 lg:p-4 lg:dark:bg-zinc-800/30 lg:text-center">
+            DEVELOPMENT IN PROGRESS..... LAZYTOADS ARE WORKING ON IT
+          </p>
+        </div>
+      </main>
+      <div>
+        {/* Your page content goes here */}
+         <Footer />
       </div>
-    </main>
-  )
-}
+    </div>
+  );
+  
+};
+
+export default Home;
