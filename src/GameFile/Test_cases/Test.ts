73f863cb87f1be65d9a27578074958341ffc4176
Mr. Cain <58246556+DrDrewCain@users.noreply.github.com>
2023-05-12 08:47:13 -0400

    remove database information inside server.ts

--- src/GameFile/Test_cases/Test.ts
+++ src/GameFile/Test_cases/Test.ts
@@ -1,41 +1,41 @@
-import { ApolloServer } from 'apollo-server-express';
-import { createTestClient } from 'apollo-server-testing';
-import { gql } from 'apollo-server';
-import { pool } from '../db';
-import resolvers from '../Utils/resolvers';
-import typeDefs from '../schema';
+// import { ApolloServer } from 'apollo-server-express';
+// import { createTestClient } from 'apollo-server-testing';
+// import { gql } from 'apollo-server';
+// import { pool } from '../db';
+// import resolvers from '../Utils/resolvers';
+// import typeDefs from '../schema';
 
-describe('resolvers', () => {
-  let server;
-  let query;
+// describe('resolvers', () => {
+//   let server;
+//   let query;
 
-  beforeAll(async () => {
-    server = new ApolloServer({
-      typeDefs,
-      resolvers,
-    });
-    query = createTestClient(server).query;
-    await pool.query('CREATE TABLE players (id SERIAL PRIMARY KEY, name TEXT, age INT)');
-  });
+//   beforeAll(async () => {
+//     server = new ApolloServer({
+//       typeDefs,
+//       resolvers,
+//     });
+//     query = createTestClient(server).query;
+//     await pool.query('CREATE TABLE players (id SERIAL PRIMARY KEY, name TEXT, age INT)');
+//   });
 
-  afterAll(async () => {
-    await pool.query('DROP TABLE players');
-    await pool.end();
-  });
+//   afterAll(async () => {
+//     await pool.query('DROP TABLE players');
+//     await pool.end();
+//   });
 
-  describe('players', () => {
-    it('should return an array of players', async () => {
-      const GET_PLAYERS = gql`
-        query {
-          players {
-            id
-            name
-            age
-          }
-        }
-      `;
-      const { data } = await query({ query: GET_PLAYERS });
-      expect(data.players).toEqual([]);
-    });
-  });
-});
+//   describe('players', () => {
+//     it('should return an array of players', async () => {
+//       const GET_PLAYERS = gql`
+//         query {
+//           players {
+//             id
+//             name
+//             age
+//           }
+//         }
+//       `;
+//       const { data } = await query({ query: GET_PLAYERS });
+//       expect(data.players).toEqual([]);
+//     });
+//   });
+// });
--- src/GameFile/server.ts
+++ src/GameFile/server.ts
@@ -1,15 +1,16 @@
 import { ApolloServer } from 'apollo-server';
 import { Pool } from 'pg';
 import typeDefs from './Utils/typeDefs';
-import resolvers from './resolvers';
+import resolvers from './Utils/resolvers';
 
 const pool = new Pool({
-  user: 'your_username',
-  host: 'your_host',
-  database: 'your_database',
-  password: 'your_password',
-  port: 5432, // change to the correct port number if necessary
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT!) : 5432,  
 });
+  
 
 const server = new ApolloServer({
   typeDefs,
