b2b5a47baa070c4d835210b137d9d13eb34edb03
Mr. Cain <58246556+DrDrewCain@users.noreply.github.com>
2023-05-12 17:40:43 -0400

    Update IN-PROGRESS-PAGE

--- WarriorsFront/src/app/page.tsx
+++ WarriorsFront/src/app/page.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-      <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-      DEVELOPMENT IN PROGRESS..... LAZYTOADS ARE WORKING ON IT
-      </p>
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:mx-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 lg:text-center">
+          DEVELOPMENT IN PROGRESS..... LAZYTOADS ARE WORKING ON IT
+        </p>
       </div>
     </main>
   )
