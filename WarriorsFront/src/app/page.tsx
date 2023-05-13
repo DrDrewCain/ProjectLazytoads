e1ce90975675cfbdc07a16426a4a96ea884758e2
Mr. Cain <58246556+DrDrewCain@users.noreply.github.com>
2023-05-12 22:47:51 -0400

    Fix the Footer

--- WarriorsFront/src/app/page.tsx
+++ WarriorsFront/src/app/page.tsx
@@ -3,11 +3,13 @@ import Footer from "./components/Footer";
 
 const Home = () => {
   return (
-    <div>
-      <main className="bg-gradient-to-r from-teal-500 to-purple-500 min-h-screen flex-1 py-10 lg:py-20">
+    <div className="flex flex-col min-h-screen">
+      <main
+        className="bg-gradient-to-r from-teal-500 to-purple-500 flex-1 py-20"
+      >
         <div className="max-w-5xl mx-auto flex flex-col items-center justify-center text-center">
           <h1
-            className="text-white text-3xl lg:text-5xl font-bold tracking-wider mb-6 italic"
+            className="text-white text-5xl font-bold tracking-wider mb-6 font-style: italic"
             style={{
               backgroundImage: "linear-gradient(to right, teal, purple)",
               WebkitBackgroundClip: "text",
@@ -17,7 +19,8 @@ const Home = () => {
           >
             Lazytoads
           </h1>
-          <p className="text-white text-base lg:text-lg mb-10"
+          <p
+            className="text-white text-lg mb-10"
             style={{
               backgroundImage: "linear-gradient(to right, teal, purple)",
               WebkitBackgroundClip: "text",
@@ -27,17 +30,20 @@ const Home = () => {
           >
             Currently working hard to bring you the best website possible.
           </p>
-          <p className="text-white text-base lg:text-lg mb-10 font-bold"
+          <p
+            className="text-white text-lg mb-10"
             style={{
               backgroundImage: "linear-gradient(to right, teal, purple)",
               WebkitBackgroundClip: "text",
               backgroundClip: "text",
               WebkitTextFillColor: "transparent",
+              fontWeight: "bold",
             }}
           >
             Releasing: 2024
-          </p>  
-          <p className="fixed left-0 bottom-0 w-full justify-center border-t border-gray-300 bg-white bg-opacity-50 pb-6 pt-8 backdrop-blur-2xl text-black font-bold tracking-wider text-base lg:text-lg lg:static lg:mx-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 lg:text-center"
+          </p>
+          <p
+            className="fixed left-0 bottom-0 w-full justify-center border-t border-gray-300 bg-white bg-opacity-50 pb-6 pt-8 backdrop-blur-2xl text-black font-bold tracking-wider lg:static lg:mx-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 lg:text-center"
             style={{
               backgroundImage: "linear-gradient(to right, teal, purple)",
               WebkitBackgroundClip: "text",
@@ -49,11 +55,7 @@ const Home = () => {
           </p>
         </div>
       </main>
-      <footer className="w-full text-center text-gray-600 text-xs lg:text-sm mt-4 lg:mt-10">
-        <p className="text-center">
-          &copy; {new Date().getFullYear()} Lazytoads. All rights reserved.
-        </p>
-      </footer>
+      <Footer />
     </div>
   );
 };
