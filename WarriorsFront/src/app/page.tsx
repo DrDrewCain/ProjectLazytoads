33a5ebe577f6c2319f248dc5d4580b69877de871
Mr. Cain <58246556+DrDrewCain@users.noreply.github.com>
2023-05-12 22:51:13 -0400

    Try to adjust for Mobile.

--- WarriorsFront/src/app/page.tsx
+++ WarriorsFront/src/app/page.tsx
@@ -4,12 +4,10 @@ import Footer from "./components/Footer";
 const Home = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <main
-        className="bg-gradient-to-r from-teal-500 to-purple-500 flex-1 py-20"
-      >
-        <div className="max-w-5xl mx-auto flex flex-col items-center justify-center text-center">
+      <main className="bg-gradient-to-r from-teal-500 to-purple-500 flex-1 py-20">
+        <div className="px-4 sm:px-6 flex flex-col items-center justify-center text-center w-full">
           <h1
-            className="text-white text-5xl font-bold tracking-wider mb-6 font-style: italic"
+            className="text-white text-4xl font-bold tracking-wider mb-6 font-style: italic"
             style={{
               backgroundImage: "linear-gradient(to right, teal, purple)",
               WebkitBackgroundClip: "text",
@@ -20,7 +18,7 @@ const Home = () => {
             Lazytoads
           </h1>
           <p
-            className="text-white text-lg mb-10"
+            className="text-white text-base mb-6"
             style={{
               backgroundImage: "linear-gradient(to right, teal, purple)",
               WebkitBackgroundClip: "text",
@@ -31,19 +29,18 @@ const Home = () => {
             Currently working hard to bring you the best website possible.
           </p>
           <p
-            className="text-white text-lg mb-10"
+            className="text-white text-base mb-6 font-bold"
             style={{
               backgroundImage: "linear-gradient(to right, teal, purple)",
               WebkitBackgroundClip: "text",
               backgroundClip: "text",
               WebkitTextFillColor: "transparent",
-              fontWeight: "bold",
             }}
           >
             Releasing: 2024
           </p>
           <p
-            className="fixed left-0 bottom-0 w-full justify-center border-t border-gray-300 bg-white bg-opacity-50 pb-6 pt-8 backdrop-blur-2xl text-black font-bold tracking-wider lg:static lg:mx-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 lg:text-center"
+            className="text-black text-base text-center border-b border-gray-300 bg-white bg-opacity-50 pb-6 pt-8 backdrop-blur-2xl font-bold tracking-wider rounded-xl"
             style={{
               backgroundImage: "linear-gradient(to right, teal, purple)",
               WebkitBackgroundClip: "text",
