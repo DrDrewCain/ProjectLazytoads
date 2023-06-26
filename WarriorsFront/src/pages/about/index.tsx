081d4e5dd627546a4519b7fdc29a285316f946e3
Mr. Cain <58246556+DrDrewCain@users.noreply.github.com>
2023-06-26 11:45:37 -0400

    Add modify about

--- WarriorsFront/src/pages/about/index.tsx
+++ WarriorsFront/src/pages/about/index.tsx
@@ -2,19 +2,27 @@ import React from "react";
 import Navigation from "../../app/components/Navigation";
 import RootLayout from "../../app/layout";
 
-const About: React.FC = () => {
+const AboutMe: React.FC = () => {
   return (
     <RootLayout>
-      <div className="flex flex-col min-h-screen">
-        <header className="bg-gradient-to-r from-teal-500 to-purple-500">
-          <Navigation />
-        </header>
+    <div className="flex flex-col min-h-screen">
+      <header className="bg-gradient-to-r from-teal-500 to-purple-500">
+        <Navigation />
+      </header>
 
-        <main className="bg-gray-100 flex-1 py-10">
-          <div className="max-w-5xl mx-auto">
-            <h2 className="text-3xl font-bold mb-4">About</h2>
-            <p className="text-black text-base">
-              Hey! I&apos;m a full-stack developer and I love to build things.
+      <main className="bg-gray-100 flex-1 py-10">
+        <div className="max-w-5xl mx-auto">
+          <h2 className="text-3xl font-bold mb-4"></h2>
+          <p
+            className="text-black text-base text-center border-b border-gray-300 bg-white bg-opacity-50 pb-6 pt-8 backdrop-blur-2xl font-bold tracking-wider rounded-xl"
+            style={{
+              backgroundImage: "linear-gradient(to right, teal, purple)",
+              WebkitBackgroundClip: "text",
+              backgroundClip: "text",
+              WebkitTextFillColor: "transparent",
+            }}
+          >
+            Hey! I&apos;m a full-stack developer and I love to build things.
               I&apos;m currently working on a few projects, including this one.
               I&apos;m passionate about learning new things and I&apos;m always
               looking for new opportunities. I&apos;m currently looking for a
@@ -29,17 +37,11 @@ const About: React.FC = () => {
               </a>{" "}
               or send me a message on the contact page
             </p>
-          </div>
-        </main>
-      </div>
+        </div>
+      </main>
+    </div>
     </RootLayout>
   );
 };
 
-export default About;
-
-
-
-
-
-
+export default AboutMe;
