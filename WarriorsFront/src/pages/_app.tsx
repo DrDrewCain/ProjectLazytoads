1b665430574b9312a3c206b30d6957e0084e5d8d
Mr. Cain <58246556+DrDrewCain@users.noreply.github.com>
2023-06-26 00:09:39 -0400

    Modify remove unnecessary stuff

--- WarriorsFront/src/pages/_app.tsx
+++ WarriorsFront/src/pages/_app.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { AppProps } from 'next/app';
-import Home from './home';
 import Navigation from '../app/components/Navigation';
 import { useRouter } from 'next/router';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
     const router = useRouter();
+    const isRootPage = router.pathname === '/';
   
     return (
       <>
