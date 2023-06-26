911002c94c4172f66f44f891ba9bc211534b5ba0
Mr. Cain <58246556+DrDrewCain@users.noreply.github.com>
2023-06-25 23:35:30 -0400

    Fix the Navigation component

--- WarriorsFront/src/app/components/Navigation.tsx
+++ WarriorsFront/src/app/components/Navigation.tsx
@@ -18,7 +18,13 @@ const Navigation = () => {
       >
         Lazytoads
       </h1>
-      <ul className="flex space-x-4 text-white font-semibold">
+      <ul className="text-white text-xl font-bold tracking-wider font-italic"
+              style={{
+                backgroundImage: "linear-gradient(to right, teal, purple)",
+                WebkitBackgroundClip: "text",
+                backgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+              }}>
         <li>
           <Link href="/home" passHref>
             Home
