19e29c8279917787b3af0a5d040e46ca9c324b75
Mr. Cain <58246556+DrDrewCain@users.noreply.github.com>
2023-06-26 13:28:16 -0400

    cursed Carousel in about page

--- WarriorsFront/src/pages/about/index.tsx
+++ WarriorsFront/src/pages/about/index.tsx
@@ -16,33 +16,39 @@ const AboutMe: React.FC = () => {
             <h2 className="text-3xl font-bold mb-4"></h2>
             <div className="profile-card">
               <div className="profile-image small">
-              <Image
-                src="/images/msturman.jpg"
-                alt="Profile"
-                className="rounded-full"
-                width={150}
-                height={150}
-              />
+                <Image
+                  src="/images/msturman.jpg"
+                  alt="Profile"
+                  className="rounded-full"
+                  width={150}
+                  height={150}
+                />
               </div>
               <div className="profile-details">
-                <h3 className="text-black text-base text-left border-gray-300 bg-white bg-opacity-50 pb-6 pt-8 backdrop-blur-2xl font-bold tracking-wider rounded-xl"
-              style={{
-                backgroundImage: "linear-gradient(to right, teal, purple)",
-                WebkitBackgroundClip: "text",
-                WebkitTextFillColor: "transparent",
-              }}>Mark Sturman</h3>
-                <p className="text-black text-base text-left border-gray-300 bg-white bg-opacity-50 pb-6 pt-8 backdrop-blur-2xl font-bold tracking-wider"
-              style={{
-                backgroundImage: "linear-gradient(to right, teal, purple)",
-                WebkitBackgroundClip: "text",
-                backgroundClip: "text",
-                WebkitTextFillColor: "transparent",
-              }}>
+                <h3
+                  className="text-black text-base text-left border-gray-300 bg-white bg-opacity-50 pb-6 pt-8 backdrop-blur-2xl font-bold tracking-wider rounded-xl"
+                  style={{
+                    backgroundImage: "linear-gradient(to right, teal, purple)",
+                    WebkitBackgroundClip: "text",
+                    WebkitTextFillColor: "transparent",
+                  }}
+                >
+                  Mark Sturman
+                </h3>
+                <h3
+                  className="text-black text-base text-left border-gray-300 bg-white bg-opacity-50 pb-6 pt-8 backdrop-blur-2xl font-bold tracking-wider"
+                  style={{
+                    backgroundImage: "linear-gradient(to right, teal, purple)",
+                    WebkitBackgroundClip: "text",
+                    backgroundClip: "text",
+                    WebkitTextFillColor: "transparent",
+                  }}
+                >
                   Full-stack Developer | Passionate about building things | Ex-Chief
-                </p>
+                </h3>
               </div>
             </div>
-            <p
+                       <p
               className="text-black text-base text-left border-gray-300 bg-white bg-opacity-50 pb-6 pt-8 backdrop-blur-2xl font-bold tracking-wider rounded-xl"
               style={{
                 backgroundImage: "linear-gradient(to right, teal, purple)",
@@ -52,12 +58,12 @@ const AboutMe: React.FC = () => {
               }}
             >
               <span className="profile-link">
-               I&apos;m a full-stack developer and I love to build things.
-              I&apos;m currently working on a few projects, including this one.
-              I&apos;m passionate about learning new things and I&apos;m always
-              looking for new opportunities. I&apos;m currently looking for a
-              full-time position as a software engineer. If you&apos;re
-              interested in working with me, please reach out to me on{" "}
+                I&apos;m a full-stack developer and I love to build things.
+                I&apos;m currently working on a few projects, including this one.
+                I&apos;m passionate about learning new things and I&apos;m always
+                looking for new opportunities. I&apos;m currently looking for a
+                full-time position as a software engineer. If you&apos;re
+                interested in working with me, please reach out to me on{" "}
                 <a
                   href="https://www.linkedin.com/in/msturman00/"
                   target="_blank"
